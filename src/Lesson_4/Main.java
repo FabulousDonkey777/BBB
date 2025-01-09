59adf3623064726ea743779b4ab308c9a7271931
FabulousDonkey777 <oceaniagb1984@gmail.com>
2025-01-09 23:14:37 +0300

    Lesson_4

--- src/Lesson_4/Main.java
+++ src/Lesson_4/Main.java
@@ -2,11 +2,11 @@ package Lesson_4;
 
 public class Main {
     public static void main(String[] args) {
-        Sotrudnik sotrudnik = new Sotrudnik("Иванов Иван Иванович", "Инженер", "ivanov@example.com", "+7 123 456 78 90", 50000, 30);
+        Sotrudnik sotrudnik = new Sotrudnik("Ivan Ivan Ivanovich", "Engineer", "ivanov@example.com", "+7 123 456 78 90", 50000, 30);
         sotrudnik.printInfo();
         System.out.println();
         Sotrudnik[] sotrudnikiArray = new Sotrudnik[5];
-        sotrudnikiArray[0] = new Sotrudnik("Дубов Иван Иванович", "Инженер-сантехник", "ivandub@mailbox.com", "892312313", 35000, 30);
+        sotrudnikiArray[0] = new Sotrudnik("Dubov Ivan Ivanovich", "Инженер-сантехник", "ivandub@mailbox.com", "892312313", 35000, 30);
         sotrudnikiArray[1] = new Sotrudnik("Петров Петр Петрович", "Менеджер", "petrov@mailbox.com", "892312313", 40000, 28);
         sotrudnikiArray[2] = new Sotrudnik("Сидоров Сидор Сидорович", "Директор", "sidorov@mailbox.com", "892312314", 60000, 45);
         sotrudnikiArray[3] = new Sotrudnik("Кузнецов Николай Николаевич", "Аналитик", "kuznetsov@mailbox.com", "892312315", 35000, 35);
