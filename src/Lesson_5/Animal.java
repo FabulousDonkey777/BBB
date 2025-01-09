05c9bcc391ea63c260618e7b28b6ee7b311e37f4
FabulousDonkey777 <oceaniagb1984@gmail.com>
2025-01-09 22:32:42 +0300

    Lesson_5

---
+++ src/Lesson_5/Animal.java
@@ -0,0 +1,106 @@
+package Lesson_5;
+class Animal {
+    private static int animalCount = 0;
+
+    public Animal() {
+        animalCount++;
+    }
+
+    public static int getAnimalCount() {
+        return animalCount;
+    }
+
+    public void run(int distance) {
+        System.out.println("Животное пробежало " + distance + " м.");
+    }
+
+    public void swim(int distance) {
+        System.out.println("Животное проплыло " + distance + " м.");
+    }
+}
+
+class Cat extends Animal {
+    private static int catCount = 0;
+    private boolean isHungry = true;
+
+    public Cat() {
+        catCount++;
+    }
+
+    public static int getCatCount() {
+        return catCount;
+    }
+
+    public void run(int distance) {
+        if (distance <= 200) {
+            System.out.println("Кот пробежал " + distance + " м.");
+        } else {
+            System.out.println("Кот не может пробежать " + distance + " м.");
+        }
+    }
+
+    public void swim(int distance) {
+        System.out.println("Кот не умеет плавать.");
+    }
+
+    public boolean eatFromBowl(FoodBowl bowl) {
+        if (isHungry && bowl.getFoodAmount() > 0) {
+            isHungry = false;
+            bowl.decreaseFood(1);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isHungry() {
+        return isHungry;
+    }
+}
+
+class Dog extends Animal {
+    private static int dogCount = 0;
+
+    public Dog() {
+        dogCount++;
+    }
+
+    public static int getDogCount() {
+        return dogCount;
+    }
+
+    public void run(int distance) {
+        if (distance <= 500) {
+            System.out.println("Собака пробежала " + distance + " м.");
+        } else {
+            System.out.println("Собака не может пробежать " + distance + " м.");
+        }
+    }
+
+    public void swim(int distance) {
+        if (distance <= 10) {
+            System.out.println("Собака проплыла " + distance + " м.");
+        } else {
+            System.out.println("Собака не может проплыть " + distance + " м.");
+        }
+    }
+}
+
+class FoodBowl {
+    private int foodAmount;
+
+    public FoodBowl(int initialFood) {
+        this.foodAmount = Math.max(initialFood, 0);
+    }
+
+    public int getFoodAmount() {
+        return foodAmount;
+    }
+
+    public void decreaseFood(int amount) {
+        foodAmount = Math.max(foodAmount - amount, 0);
+    }
+
+    public void addFood(int amount) {
+        foodAmount += amount;
+    }
+}
---
+++ src/Lesson_5/Main.java
@@ -0,0 +1,49 @@
+package Lesson_5;
+
+public class Main {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat();
+        Cat cat2 = new Cat();
+        Dog dog1 = new Dog();
+
+        cat1.run(150);
+        cat2.run(250);
+        dog1.run(400);
+
+        cat1.swim(5);
+        dog1.swim(5);
+
+        FoodBowl bowl = new FoodBowl(5);
+        System.out.println("Еды в миске: " + bowl.getFoodAmount());
+
+        cat1.eatFromBowl(bowl);
+        System.out.println("Кот 1 сыт: " + !cat1.isHungry());
+        System.out.println("Еды в миске: " + bowl.getFoodAmount());
+
+        cat2.eatFromBowl(bowl);
+        System.out.println("Кот 2 сыт: " + !cat2.isHungry());
+        System.out.println("Еды в миске: " + bowl.getFoodAmount());
+
+        System.out.println("Всего животных: " + Animal.getAnimalCount());
+        System.out.println("Всего котов: " + Cat.getCatCount());
+        System.out.println("Всего собак: " + Dog.getDogCount());
+
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+
+        Shape circle = new Circle(5, "Red", "Black");
+        Shape rectangle = new Rectangle(4, 6, "Blue", "Green");
+        Shape triangle = new Triangle(3, 4, 5, "Yellow", "Purple");
+
+        circle.printDetails();
+        System.out.println();
+
+        rectangle.printDetails();
+        System.out.println();
+
+        triangle.printDetails();
+    }
+}
